976d8d4e5178ccd3fd482ba6e277fc1117071026
Camilo López <camiloj.lopez79@gmail.com>
2022-08-16 09:25:26 -0400

    Implemented auth register controller

--- src/controllers/auth/authRegister.controller.ts
+++ src/controllers/auth/authRegister.controller.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+// Models
+import { User } from '../../models';
 
+/*
+  PATH: '/api/auth/register'
+  REQUIRED-JWT: false
+*/
 export const authRegister = async ( req: Request, res: Response ) => {
+  const { name, email, password } = req.body;
+
   try {
+    const userRegistered = new User({ name, email, password });
+
+    // Encrypt password
+    const salt = bcrypt.genSaltSync();
+    userRegistered.password = bcrypt.hashSync( password, salt );
+    // Save to DB
+    await userRegistered.save();
+
+    //TODO: Generate JWT
 
     res.status( 201 ).json({
       ok: true,
-      msg: 'authRegister'
+      user: userRegistered
     });
 
   } catch ( err ) {
